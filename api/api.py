af9dd629e2f55055215eacb0ccffcede31c0df5e
Mustapha Essafi <mustapha.essafi.dev@gmail.com>
2026-01-08 18:06:22 +0100

    test 6

--- api/api.py
+++ api/api.py
@@ -146,4 +146,4 @@ def log_data():
 # ✅ دابا: debug=False
 # ======================================================
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run(host="127.0.0.1", port=5000, debug=False)
